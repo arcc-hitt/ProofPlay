1bc2fe56a13628011cd63873a0b0355df8c8c181
Archit Mahule <archit.mahule@mathongo.com>
2025-04-22 22:54:13 +0530

    Add progress route api

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/connectDB';
-// import progressRoutes from './routes/progressRoutes';
+import progressRoutes from './routes/progressRoutes';
 import dotenv from 'dotenv';
 dotenv.config(); 
 
@@ -12,7 +12,7 @@ app.use(express.json());
 const { MONGO_URI, PORT } = process.env;
 connectDB(MONGO_URI!);
 
-// app.use('/api/progress', progressRoutes);
+app.use('/api/progress', progressRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
