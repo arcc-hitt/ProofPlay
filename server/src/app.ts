fc66d909a8bc6aca95195e6710342b1c5c6dc68b
Archit Mahule <archit.mahule@mathongo.com>
2025-04-22 22:02:04 +0530

    Initialize backend server, structure and dependencies

---
+++ server/src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import { connectDB } from './config/connectDB';
+// import progressRoutes from './routes/progressRoutes';
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const { MONGO_URI, PORT } = process.env;
+connectDB(MONGO_URI!);
+
+// app.use('/api/progress', progressRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
